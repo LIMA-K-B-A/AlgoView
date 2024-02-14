f3d18aeba3afb2799bfc7b0f2738eaea55f47b3c
Kaique Brayan <kaiquebrayan007@gmail.com>
2024-02-14 13:29:17 -0300

    DarkMode adicionado em todas as páginas

--- assets/script/pilhas.js
+++ assets/script/pilhas.js
@@ -1,3 +1,22 @@
+const modeButton = document.getElementById('mode_button');
+
+modeButton.addEventListener('click', () => {
+    const body = document.getElementById('index_page');
+    const modeIcon = document.getElementById('mode_icon');
+
+    if (modeIcon.classList.contains('fa-moon')) {
+        modeIcon.classList.remove('fa-moon');
+        modeIcon.classList.add('fa-sun');
+
+        body.classList.add('dark');
+    } else {
+        modeIcon.classList.remove('fa-sun');
+        modeIcon.classList.add('fa-moon');
+
+        body.classList.remove('dark');
+    }
+});
+
 const stack = [];
 
   function atualizarPilha() {
